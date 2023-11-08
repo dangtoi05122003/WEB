dcb60fcb8e97c57f2f45279d103f91c0489e4e8c
dangtoi05122003 <142383371+dangtoi05122003@users.noreply.github.com>
2023-11-08 16:04:56 +0700

    p1

---
+++ FILE6-PHPPOST.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DXT</title>
+</head>
+<body>
+    welcome <?php echo $_POST["name"]; ?><br>
+    your email address is: <?php echo $_POST["email"]; 
+</body>
+</html>
