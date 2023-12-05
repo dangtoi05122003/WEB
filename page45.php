7bf4b4f3036360da0c0a5c9160ec4f5965b52875
dangtoi05122003 <142383371+dangtoi05122003@users.noreply.github.com>
2023-12-05 23:59:29 +0700

    slide 1

---
+++ page45.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DXT</title>
+</head>
+<body>
+    <?php
+        require 'connect.php';
+        $sql = "INSERT INTO flights (Origin,Destionation,Duration) VALUES ('Hà Nội', 'Paris', '4000')";
+    ?>
+</body>
+</html>
